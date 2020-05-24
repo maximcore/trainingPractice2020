9a87b9420f7a623c96fde0154d1ca6d8e00cb6c7
Maxim Borisenko <maximbeducation@gmail.com>
2020-05-24 13:23:37 +0300

    add task 5

--- task 5/src/GetName.java
+++ task 5/src/GetName.java
@@ -4,14 +4,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "FirstServlect")
-public class FirstServlect extends HttpServlet {
+@WebServlet(name = "GetName")
+public class GetName extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String req = request.getParameter("name");
+        PrintWriter pw = response.getWriter();
+        pw.println(req);
     }
 }
